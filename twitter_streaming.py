4cf539bd82f63d924477fe9977d0356da4a2c854
Nanra <sukedynanra@gmail.com>
2017-10-18 09:55:43 +0700

    Update Keywords

--- twitter_streaming.py
+++ twitter_streaming.py
@@ -2,7 +2,6 @@
 from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
-import json
 
 # Variables that contains the user credentials to access Twitter API
 access_token = "151969580-1pY75HNLMl351Bhn4Ha2SCKQWT1fwWrd58PNBMi2"
@@ -14,20 +13,19 @@ consumer_secret = "QvBgFj6ViORKnbpYjrl81Eve2Qg9zlMvjjGFCmlX7Z3ea9fYpK"
 # This is a basic listener that just prints received tweets to stdout.
 class StdOutListener(StreamListener):
     def on_data(self, data):
-        decoded = json.loads(data)
-        print (decoded['user']['screen_name'], decoded['text'].encode('ascii', 'ignore'))
-        # print data
+        print data
         return True
 
     def on_error(self, status):
         print status
 
+
 if __name__ == '__main__':
     # This handles Twitter authentification and the connection to Twitter Streaming API
-    lines = StdOutListener()
+    l = StdOutListener()
     auth = OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
-    stream = Stream(auth, lines)
+    stream = Stream(auth, l)
 
     # This line filter Twitter Streams to capture data by the keywords: 'python', 'javascript', 'ruby'
-    stream.filter(track=['dki', 'jakarta', 'anies', 'sandi'])
+    stream.filter(track=['ridwan', 'kamil', 'dedi', 'mulyadi', 'dedy', 'mizwar', 'pilkada', 'jabar'])
